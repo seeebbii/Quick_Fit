1cc57ba6e28f9e18971519b44c592a1955f12591
Arshman Afzal <arshman3@gmail.com>
2020-06-30 10:55:35 -0700

    Added Login and Register UI

--- app/src/main/java/com/example/quickfit/MainActivity.java
+++ app/src/main/java/com/example/quickfit/MainActivity.java
@@ -1,14 +1,40 @@
 package com.example.quickfit;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Pair;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ImageButton btRegister;
+    private TextView tvLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btRegister  = findViewById(R.id.btRegister);
+        tvLogin     = findViewById(R.id.tvLogin);
+        btRegister.setOnClickListener(this);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    public void onClick(View v) {
+        if (v==btRegister){
+            Intent intent   = new Intent(MainActivity.this,RegisterActivity.class);
+            Pair[] pairs    = new Pair[1];
+            pairs[0] = new Pair<View,String>(tvLogin,"tvLogin");
+            ActivityOptions activityOptions = ActivityOptions.makeSceneTransitionAnimation(MainActivity.this,pairs);
+            startActivity(intent,activityOptions.toBundle());
+        }
     }
 }
